0ee422400fe996b9fbb796e8e53dfe882600d38e
Polina Kirpichonak <pkirpichenok@gmail.com>
2025-07-10 12:50:54 +0300

    add data for db, migrations

---
+++ store/Entities/Game.cs
@@ -0,0 +1,14 @@
+namespace store.Entities;
+
+public class Game
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+
+    public int GenreID { get; set; }
+    public Genre? Genre { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateOnly ReleaseDate { get; set; }
+}
--- store/Program.cs
+++ store/Program.cs
@@ -1,6 +1,13 @@
+using store.Data;
 using store.DTOs;
 using store.Endpoints;
+using store.Entities;
 var builder = WebApplication.CreateBuilder(args);
+
+
+//Connecting the database to app
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
